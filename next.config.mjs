1458415a7a22881c79411b1be8c8184f269cae3d
sugitaku222719 <sugitaku222719@gmail.com>
2024-10-18 14:46:59 +0900

    本番環境(out)の作成

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_APIKEY: process.env.NEXT_PUBLIC_APIKEY,
+    NEXT_PUBLIC_AUTHDOMAIN: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+    NEXT_PUBLIC_PROJECTID: process.env.NEXT_PUBLIC_PROJECTID,
+    NEXT_PUBLIC_STORAGEBUCKET: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+    NEXT_PUBLIC_MESSAGINGSENDERID: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+    NEXT_PUBLIC_APPID: process.env.NEXT_PUBLIC_APPID
+  }
 };
+
 
 export default nextConfig;
