25c8beccd6a24e4bba14339d386911c5ab30bd11
sugitaku222719 <sugitaku222719@gmail.com>
2024-10-24 19:35:46 +0900

    対戦部屋の作成

---
+++ src/components/RoomCreate.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import _Header from './_Header'
+import _RoomCreate from './_RoomCreate'
+
+function RoomCreate() {
+  return (
+    <div>
+      <_Header />
+      <h1>ルーム作成</h1>
+      <_RoomCreate />
+    </div>
+  )
+}
+
+export default RoomCreate
---
+++ src/components/_RoomCreate.js
@@ -0,0 +1,13 @@
+import { auth } from '@/lib/Firebase'
+import React from 'react'
+
+function _RoomCreate() {
+  return (
+    <div>
+      <div>あなたのuid　:　{auth.currentUser.uid}</div>
+      <div>あなたが使用するデッキ名と対戦相手のuidを入力してください</div>
+    </div>
+  )
+}
+
+export default _RoomCreate
