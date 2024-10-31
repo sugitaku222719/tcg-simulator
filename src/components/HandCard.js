dcedbd902dd5c1570a89182504bdd02aba080025
sugitaku222719 <sugitaku222719@gmail.com>
2024-10-31 15:59:13 +0900

    相手の手札をクリックしたときのエラー解消

--- src/components/HandCard.js
+++ src/components/HandCard.js
@@ -3,16 +3,24 @@ import styles from "@/styles/HandCard.module.css";
 
 const HandCard = ({ card, addFieldCard, onRightClick }) => {
   const handleContextMenu = (e) => {
-    e.preventDefault(); // 右クリックのデフォルトメニューを無効化
-    onRightClick(card); // カード情報を渡して関数を呼び出す
+    e.preventDefault();
+    if (onRightClick) {
+      onRightClick(card);
+    }
+  };
+
+  const handleClick = () => {
+    if (addFieldCard) {
+      addFieldCard(card);
+    }
   };
 
   return (
     <div
       className={styles.card}
-      draggable={true}
-      onClick={() => addFieldCard(card)}
-      onContextMenu={handleContextMenu} // 右クリック時のイベント
+      draggable={addFieldCard ? true : false}
+      onClick={handleClick}
+      onContextMenu={handleContextMenu}
     >
       <div>{card.cardName}</div>
       <div>
