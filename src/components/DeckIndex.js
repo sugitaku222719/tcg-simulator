86917431170798689517f1d9f136958341e06b07
sugitaku222719 <sugitaku222719@gmail.com>
2024-11-01 20:03:05 +0900

    デッキ登録画面のcss、mui

--- src/components/DeckIndex.js
+++ src/components/DeckIndex.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import _Header from './_Header'
-import _DeckIndex from './_DeckIndex'
-import Link from 'next/link'
+import React from 'react';
+import _Header from './_Header';
+import _DeckIndex from './_DeckIndex';
+import Link from 'next/link';
+import { Container, Typography, Button } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import styles from '@/styles/DeckIndex.module.css';
 
 function DeckIndex() {
   return (
-    <div>
+    <div className={styles.deckIndexPage}>
       <_Header />
-      <h1>デッキ登録画面</h1>
-      <Link href = "/deckRegistration/newDeck" ><button>New deck create</button></Link>
-      <_DeckIndex />
+      <Container maxWidth="lg" className={styles.container}>
+        <Typography variant="h2" component="h1" className={styles.title}>
+          デッキ登録画面
+        </Typography>
+        <Link href="/deckRegistration/newDeck" passHref>
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<AddIcon />}
+            className={styles.newDeckButton}
+          >
+            New deck create
+          </Button>
+        </Link>
+        <_DeckIndex />
+      </Container>
     </div>
-  )
+  );
 }
 
-export default DeckIndex
+export default DeckIndex;
