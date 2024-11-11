aefb36f5c94c404d596bd45e6ed9436807bf2cad
sugitaku222719 <sugitaku222719@gmail.com>
2024-11-11 18:05:04 +0900

    DeckEdit画面におけるアラート

--- src/pages/deckRegistration/[deckDocId].js
+++ src/pages/deckRegistration/[deckDocId].js
@@ -1,21 +1,29 @@
-import SignIn from '@/components/SignIn'
 import { auth } from '@/lib/Firebase';
 import { useAuthState } from "react-firebase-hooks/auth";
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DeckEdit from '@/components/DeckEdit';
+import Entrance from '@/components/Entrance';
 import { useRouter } from 'next/router';
 
 function DeckNamePage({ decDocId }) {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const router = useRouter();
+  const [isAlertShown, setIsAlertShown] = useState(false);
 
-  if (router.isFallback) {
-    return <div>Loading...</div>
+  useEffect(() => {
+    if (!loading && !user && !isAlertShown) {
+      setIsAlertShown(true);
+      alert("ログインしてください");
+    }
+  }, [user, loading, isAlertShown]);
+
+  if (loading || router.isFallback) {
+    return <div>Loading...</div>;
   }
 
   return (
     <div>
-      {user ? <DeckEdit decDocId={decDocId} /> : <SignIn />}
+      {user ? <DeckEdit decDocId={decDocId} /> : <Entrance />}
     </div>
   )
 }
